ebb5b0e70ef10025f81656863336274c7ac29f02
Tslachim <119934199+Tslachim@users.noreply.github.com>
2023-02-20 10:52:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,75 +12,69 @@ import html
 from requests import get
 from bs4 import BeautifulSoup as bs
 
-# Scraper pro informace -->> vypíše mi to ze zvolené stránky kódy a města 
-def scraper_city_info(web: str,) -> dict: 
+response = get(sys.argv[1])
+soup = bs(response.text, features="html.parser")
+
+def scraper_city_info(soup) -> dict:                                       # Scraper pro informace -->> vypíše mi to ze zvolené stránky kódy a města 
     """ Get city Id and names """
-    response = get(web)
-    soup = bs(response.text, features="html.parser")
     selection_code = soup.find_all(name= "td", class_= "cislo")
     codes = [code.getText() for code in selection_code]
     selection_city = soup.find_all(name= "td", class_= "overflow_name")
-    if selection_city == []:                                                # Ošetření kvůli Brnu :D
+    if selection_city == []:                                                    # Ošetření kvůli Brnu :D
         selection_city = soup.find_all(name= "td", headers="t1sa1 t1sb2") 
     cities = [code.getText() for code in selection_city]
     return codes, cities
 
-codes = scraper_city_info(sys.argv[1])[0] 
-cities = scraper_city_info(sys.argv[1])[1]                                  # změnit na sys.argv[1]
+codes = scraper_city_info(soup)[0] 
+cities = scraper_city_info(soup)[1]
 
-# Spojení odkazů webů 
-def conection_links(new_web_pages):
+def conection_links(new_web_pages):                                             # Spojení odkazů webů
     web_main = "https://www.volby.cz/pls/ps2017nss/"
     city_web = [(web_main+web) for web in new_web_pages]
     return city_web
 
-# Získám odkazy k městům 
-def next_web():
-    web = sys.argv[1]                                                        # taky potřeba změnit na sys.argv[1]
-    response = get(web)
+def next_web():                                                                 # Získám odkazy k městům
+    web = sys.argv[1]
     soup = bs(response.content, "html.parser")
     adress_endings = [one_td.find('a')['href'] for one_td in soup.find_all('td', class_='cislo') if one_td.find('a')]
     return conection_links(adress_endings)
 
 city_web = next_web()
 
-# Hlavní popisky Id, město .... pro hlavičku .csv souboru 
-def get_title():  
+def get_title():                                                                # Hlavní popisky Id, město .... pro hlavičku .csv souboru
     """ Get names for Title in .csv """
     titles = ["Code", "Location", "Registred", "Envelopes", "Valid"]
-    response = get(city_web[0])                                              # libovolná stránka z těcho stranek, chci jen názvy stran
+    response = get(city_web[0])                                                 # libovolná stránka z těcho stranek, chci jen názvy stran
     soup = bs(response.text, features="html.parser")
     selection = soup.find_all(name= "td", class_= "overflow_name")
     groups = [group.getText() for group in selection]
     headers = titles + groups
     return headers
 
-headers = get_title()                                                         # nadpisy pro soubor csv
+headers = get_title()
 
-# Pro každý odkaz potřebuji udělat scraper informací k danému městu
-def all_city_info():
+def all_city_info():                                                            # Pro každý odkaz potřebuji udělat scraper informací k danému městu
     all_city_information = []
     for index in range(len(city_web)):
-        response = get(city_web[index -1])                                     # tady místo odkazu přiřadit city_web (získané stránky)
+        response = get(city_web[index -1])
         soup = bs(response.content, "html.parser")
         information_city = []
 
-        # získání prvního čísla počet voličů
-        number = (soup.find("td", headers="sa2" )).getText()
+        
+        number = (soup.find("td", headers="sa2" )).getText()                    # získání prvního čísla počet voličů
         number = html.unescape(number)
         information_city.append(number)
 
-        # získání počtu obálek 
-        number_1 = (soup.find("td", headers="sa3" )).getText()
+        
+        number_1 = (soup.find("td", headers="sa3" )).getText()                  # získání počtu obálek 
         number_1= html.unescape(number_1)
         information_city.append(number_1)
 
-        # získání neplatných hlasů 
-        number_2 = (soup.find("td", headers="sa6" )).getText()
+        
+        number_2 = (soup.find("td", headers="sa6" )).getText()                   # získání neplatných hlasů 
         number_2 = html.unescape(number_2)
         information_city.append(number_2)
 
-        # print(information_city[0], information_city[1], information_city[2])
         for num in soup.find_all("td", headers="t1sa2 t1sb3"):
             information_city.append(html.unescape(num.getText()))
 
@@ -91,10 +85,9 @@ def all_city_info():
 
 all_city_information = all_city_info()
 
-# přidání codes a cities do daného listu 
-if len(all_city_information) > 1:
+if len(all_city_information) > 1:                                                # přidání codes a cities do daného listu
     for index in range(len(all_city_information)):   
-        all_city_information[index - 1].insert(0, codes[index - 1])             # napsat pokud je len(all_city_information pro Brno (zatím nefunguje)!! myslím si že je malý index nebo tak něco)
+        all_city_information[index - 1].insert(0, codes[index - 1])
         all_city_information[index - 1].insert(1, cities[index - 1])
 else: 
     all_city_information[0].insert(0, codes[0])
@@ -102,6 +95,7 @@ else:
 
 with open(sys.argv[2], mode="w", encoding='utf-8', newline='') as new:
     writer = csv.writer(new, delimiter=';')
+    print(f"Vytvářím soubor {sys.argv[2]} a zapisuji do něj data")
     writer.writerow(headers)
     for line in all_city_information:
         writer.writerow(line)
